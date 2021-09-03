10817e4737e15ec7e420221f4b28caf4dfe9b8a7
Guilherme Rohr <rohrgm@gmail.com>
2021-09-03 09:12:46 -0300

    Initial commit

---
+++ Atividade1/carros/src/main/resources/Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO carro VALUES('9BBNSZPPA288003333', 2012, 'branca', 'ford', 'fiesta', 10000);
+INSERT INTO carro VALUES('9BBNSZPPA288003334', 2015, 'branca', 'volkswagen', 'gol', 30000);
+INSERT INTO carro VALUES('9BBNSZPPA288003335', 2016, 'branca', 'volkswagen', 'gol', 20000);
+INSERT INTO carro VALUES('9BBNSZPPA288003336', 2011, 'preta', 'ford', 'fiesta3', 50000);
+INSERT INTO carro VALUES('9BBNSZPPA288003337', 2011, 'preta', 'ford', 'fiesta4', 50000);
